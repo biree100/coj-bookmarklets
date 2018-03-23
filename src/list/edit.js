11b2193065c333b778a21664813d0fa801ccf7ed
biree100 <jumpei.okada@mvrck.co.jp>
2018-03-24 03:02:00 +0900

    Refactor and Implementing RarityCheck

--- src/list/edit.js
+++ src/list/edit.js
@@ -2,43 +2,34 @@
 // edit.js
 //
 // Set the argument Array as global;
-// ex) editArgs = [3, 1, 0, 0, 0, 0];
+// ex) editArgs = [3, undefined, 0, 0, 0];
 //
 // editArgs[0] -> editMode
 //   0: default
 //   1: all
 //   2: excess
 //   3: shortage
-// editArgs[1] -> ignoreOld
-//   0: default
-//   1: show only standard-available cards
+// **REMOVED** editArgs[1] -> ignoreOld
 // editArgs[2] -> hideCards
 //   0: default
 //   1: hide cards when click
 // editArgs[3] -> iPad
 //   0: default
-//   1: for iPad
+//   1: for iPads
 // editArgs[4] -> sortType
 //   0: default
-//   1: sort with card type
-//   2: sort with rarity
-// editArgs[5] -> dropRarity
-//   0: default
-//   1: drop C
-//   2: drop UC or lower rarity
-//   3: drop R  or lower rarity
-//   4: drop VR or lower rarity
+//   1: sort by card types
+//   2: sort by rarities
+// **REMOVED** editArgs[5] -> dropRarity
 //
 
 
 (function() {
   var args = editArgs || [];
   var editMode = args[0] || 0;
-  var ignoreOld = args[1] || 0;
   var hideCards = args[2] || 0;
   var iPad = args[3] || 0;
   var sortType = args[4] || 0;
-  var dropRarity = args[5] || 0;
 
   var cc = cardbox_card;
   var hist = [];
@@ -100,25 +91,24 @@
     cc.innerHTML = cardData.map(
       function(e) {
         var n = getN(e);
-        if (n && checkCard(e)) return '<li id="cb_' + e.id + '"><div class="card_image'
-          + (e.sprite ?
+        if (n && checkCard(e)) return '<li id="cb_' + e.id + '"><div class="card_image' +
+          (e.sprite ?
             ' ' + e.imgClass :
             '" style="background:url(' + e.s_img + ') no-repeat;background-size:100%;'
-          ) + '"></div><div class="foil' + (foilConf && ' display_block')
-          + '"></div><div class="darken"></div><div class="card_quantity">x' + n + '</div></li>';
+          ) +
+          '"></div><div class="foil' + (foilConf && ' display_block') +
+          '"></div><div class="darken"></div><div class="card_quantity">x' + n + '</div></li>';
         else return '';
       }
     ).join('');
 
     function checkCard(card) {
-      return eval('dop_' + card.op).checked
-        && eval('type_' + card.type).checked
-        && eval('color_' + card.color).checked
-        && (card.species ? eval('species_' + card.species).checked : species_all.checked)
-        && !((ignoreOld || !national_all.checked) && card.ver < 6)
-        && (card.ver == prVer || card.ver == spVer || eval((card.ver < 6 ? 'national_' : 'ver_') + card.ver).checked)
-        && eval('cost_' + Math.min(card.cost, 8)).checked
-        && card.rarity > dropRarity;
+      return ['dop_' + card.op, 'type_' + card.type, 'color_' + card.color, 'rarity_' + card.rarity, 'cost_' + Math.min(card.cost, 8)].every(check) &&
+        (species_all.checked || check('species_' + card.species)) &&
+        (card.ver == prVer || card.ver == spVer || check((card.ver < 6 ? 'national_' : 'ver_') + card.ver));
+      function check(boxId) {
+        return (document.getElementById(boxId) || {}).checked;
+      }
     }
 
     function getSortedMasterCard() {
@@ -133,24 +123,26 @@
     }
 
     function comp() {
+      var byNumber, byCardType, byRarity;
       return {
-        0: function(a, b) {
+        0: byNumber = function(a, b) {
           return a.view_no.localeCompare(b.view_no);
         },
-        1: function(a, b) {
-          return Math.max(a.type, 2) - Math.max(b.type, 2) || a.color - b.color || a.view_no.localeCompare(b.view_no);
+        1: byCardType = function(a, b) {
+          return Math.max(a.type, 2) - Math.max(b.type, 2) || a.color - b.color || byNumber(a, b);
         },
-        2: function(a, b) {
-          return b.rarity - a.rarity || Math.max(a.type, 2) - Math.max(b.type, 2) || a.color - b.color || a.view_no.localeCompare(b.view_no);
+        2: byRarity = function(a, b) {
+          return b.rarity - a.rarity || byCardType(a, b);
         }
       }[sortType];
     }
 
     function getN(e) {
+      var pos = possessions[e.id + foilConf] || 0;
       return {
-        1: possessions[e.id + foilConf],
-        2: Math.max((possessions[e.id + foilConf] || 0) - 3, 0),
-        3: Math.max(3 - (possessions[e.id + foilConf] || 0), 0)
+        1: pos,
+        2: Math.max(pos - 3, 0),
+        3: Math.max(3 - pos, 0)
       }[editMode];
     }
 
